e37e4e82da0bcb1de43fea4df644dfa7e18cc9cf
finnqiao <yu.qiao@tufts.edu>
2019-04-20 16:13:16 -0500

    Switching to market instead of categories

--- src/clean_companies_data.py
+++ src/clean_companies_data.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import missingno as msno
 import collections
 from sklearn.preprocessing import MultiLabelBinarizer
+import itertools
+from tqdm import tqdm
 
 # Import and inspect initial data on companies.
 company_df = pd.read_csv('./data/external/companies.csv')
-
-company_df.head()
 
 # Visualize missing values in data.
 msno.matrix(company_df)
@@ -20,7 +20,21 @@ company_df.shape
 company_df.columns
 
 # Keep columns that are relevant.
-company_df = company_df[['permalink', 'name', 'category_list', 'funding_total_usd', 'status', 'country_code', 'city', 'funding_rounds', 'founded_month', 'founded_quarter', 'founded_year', 'first_funding_at', 'last_funding_at']]
+company_df = company_df[['permalink', 'name', 'market', 'category_list', 'funding_total_usd', 'status', 'country_code', 'city', 'funding_rounds', 'founded_month', 'founded_quarter', 'founded_year', 'first_funding_at', 'last_funding_at']]
+
+# Lower case if a column is of string type.
+company_df = company_df.apply(lambda column: column.str.lower() if pd.api.types.is_string_dtype(column) else column)
+
+# Check number of unique market types.
+len(company_df['market'].unique())
+
+# See list of top 50 market types by company counts.
+company_df['market'].value_counts().nlargest(50)
+
+
+
+
+
 
 # Drop missing values for category list as imputation methods are hard to apply for one hot encoding here.
 company_df = company_df.dropna(subset=['category_list'])
@@ -39,7 +53,8 @@ cat_one_hot.head()
 [(key, len(list(group))) for key, group in itertools.groupby(company_df['category_list'].sum())]
 
 counter = collections.Counter(company_df['category_list'].sum())
-counter.most_common(100)
+len(counter)
+counter.most_common(50)
 
 
 
