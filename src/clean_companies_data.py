773adbff86b3373f24ed7fe7d26033e7c4c9e502
finnqiao <yu.qiao@tufts.edu>
2019-05-14 12:01:32 -0500

    Script to clean companies data and generate new features

--- src/clean_companies_data.py
+++ src/clean_companies_data.py
@@ -1,169 +1,205 @@
 import logging
 import os
+import re
+import argparse
+
+import yaml
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import missingno as msno
-import collections
-import itertools
-from tqdm import tqdm
-import nltk
-from datetime import datetime, timedelta
-import csv
 
-# Import and inspect initial data on companies.
-company_df = pd.read_csv('./data/external/companies.csv')
+import src.helpers.gen_helpers as gen_h
+import src.helpers.nlp_helpers as nlp_h
 
-# Visualize missing values in data.
-msno.matrix(company_df)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
 
-# Drop if market is empty
-company_df = company_df.dropna(subset = ['market'])
+def read_data(path):
+    """Read csv file in from path after being downloaded from S3"""
+    df = pd.read_csv(path)
+    return df
 
-# Basic dataframe shape and columns.
-company_df.shape
+def filter_columns(df, column_subset, na_subset):
+    """Filter out irrelevant columns and drop if values are NaN for a subset of
+    columns"""
+    df = df[column_subset]
+    df = df.dropna(subset = na_subset)
+    df = df.apply(lambda column: (column.str.lower() if
+    pd.api.types.is_string_dtype(column) else column))
+    return df
 
-company_df.columns
+def reduce_market_categories(df):
+    """Reduce market categories to top 50 and other with nlp techniques"""
+    # Save list of all market types.
+    market_types = list(df['market'].unique())
 
-# Keep columns that are relevant.
-company_df = company_df[['permalink', 'name', 'market', 'category_list', 'funding_total_usd', 'status', 'country_code', 'funding_rounds', 'founded_at', 'founded_month', 'founded_quarter', 'founded_year', 'first_funding_at', 'last_funding_at']]
+    # Save list of top 50 market types by company counts.
+    top_50_markets = list(df['market'].value_counts().nlargest(50).index)
 
-# Lower case if a column is of string type.
-company_df = company_df.apply(lambda column: column.str.lower() if pd.api.types.is_string_dtype(column) else column)
+    outside_50_markets = [x for x in market_types if x not in top_50_markets]
 
-# Check number of unique market types.
-len(company_df['market'].unique())
+    # Create dictionary of top 50 markets.
+    top_50_dict = dict.fromkeys(top_50_markets)
 
-# Save list of all market types.
-market_types = list(company_df['market'].unique())
+    # First pass at collapsing market categories.
+    # Done by checking if first word in a category is present in other category
+    # labels.
+    # Example 1: word "advertising" apppears in "advertising platforms".
+    # Example 2: "enterprise" is the first word in "enterprise software"
+    # appears in "enterprise 2.0"
 
-# Save list of top 50 market types by company counts.
-top_50_markets = list(company_df['market'].value_counts().nlargest(50).index)
+    for key in tqdm(top_50_dict.keys()):
+        # Initialize each key with empty list
+        top_50_dict[key] = []
+        for cat in outside_50_markets:
+            # Check if first word is present in category, add to key value list,
+            # remove from category list.
+            if key.split(' ')[0] in cat.split(' '):
+                top_50_dict[key].append(cat)
+                outside_50_markets.remove(cat)
+            elif ((len(key.split(' ')) > 1) and (key.split(' ')[1] == 'and')
+            and (key.split(' ')[2] in cat.split(' '))):
+                top_50_dict[key].append(cat)
+                outside_50_markets.remove(cat)
 
-outside_50_markets = [x for x in market_types if x not in top_50_markets]
+    # Check how many categories are left.
+    remainder_outside_50 = outside_50_markets
 
-# Create dictionary of top 50 markets.
-top_50_dict = dict.fromkeys(top_50_markets)
+    # Second pass at collapsing market categories.
+    # Market category labels are split into character n-grams.
+    # N-gram similarity Dice metric is calculated with intersection/union of
+    # n-gram sets.
+    for key in tqdm(top_50_dict.keys()):
+        for cat in remainder_outside_50:
+            if sim4gram(key, cat) > 0.25:
+                top_50_dict[key].append(cat)
+                remainder_outside_50.remove(cat)
 
-# First pass at collapsing market categories.
-# Done by checking if first word in a category is present in other category labels.
-# Example 1: word "advertising" apppears in "advertising platforms".
-# Example 2: "enterprise" is the first word in "enterprise software" apppears in "enterprise 2.0"
+    # Assign remainder of categories to "Other"
+    top_50_dict['other'] = remainder_outside_50
 
-for key in tqdm(top_50_dict.keys()):
-    # Initialize each key with empty list
-    top_50_dict[key] = []
-    for cat in outside_50_markets:
-        # Check if first word is present in category, add to key value list, remove from category list.
-        if key.split(' ')[0] in cat.split(' '):
-            top_50_dict[key].append(cat)
-            outside_50_markets.remove(cat)
-        elif (len(key.split(' ')) > 1) and (key.split(' ')[1] == 'and') and (key.split(' ')[2] in cat.split(' ')):
-            top_50_dict[key].append(cat)
-            outside_50_markets.remove(cat)
+    # Invert keys and values
+    category_replace_dict = invert_dict(top_50_dict)
 
-# Check how many categories are left.
-remainder_outside_50 = outside_50_markets
-len(remainder_outside_50)
+    # Relabel key value pairs that do not match well.
+    category_replace_dict['big data analytics'] = 'big data'
+    category_replace_dict['coworking'] = 'other'
+    category_replace_dict['mining technologies'] = 'other'
+    category_replace_dict['mobility'] = 'other'
+    category_replace_dict['nanotechnology'] = 'other'
+    category_replace_dict['real time'] = 'other'
+    category_replace_dict['space travel'] = 'other'
+    category_replace_dict['social network media'] = 'social media'
 
-# Second pass at collapsing market categories.
-# Market category labels are split into character n-grams.
-# N-gram similarity Dice metric is calculated with intersection/union of n-gram sets.
+    # Replace values in market column based on replacement dictionary.
+    df['market'] = df['market'].map(category_replace_dict).fillna(df['market'])
 
-def word2ngrams(text, n=4, exact=True):
-    """Convert text into ngrams."""
-    return ["".join(g) for g in zip(*[text[i:] for i in range(n)])]
+    return df
 
-def sim4gram(text1, text2):
-    """Calculate ngram similarity metric."""
-    list1 = word2ngrams(text1)
-    list2 = word2ngrams(text2)
-    return len(list(set(list1) & set(list2)))/len(list(set(list1) | set(list2)))
+def generate_onehot_features(df, column):
+    """Generate one-hot encoding of selected column"""
+    df[column].value_counts()
 
-for key in tqdm(top_50_dict.keys()):
-    for cat in remainder_outside_50:
-        if sim4gram(key, cat) > 0.25:
-            top_50_dict[key].append(cat)
-            remainder_outside_50.remove(cat)
+    df = pd.concat([df, pd.get_dummies(df[column], prefix=column)], axis=1)
+    df = df.drop(column, axis=1)
 
-# Check how many categories are left.
-len(remainder_outside_50)
+    return df
 
-# Assign remainder of categories to "Other"
-top_50_dict['other'] = remainder_outside_50
+def company_country_features(df):
+    """Generate one-hot encoding of top countries and others"""
+    #  Countries with startup numbers above global mean.
+    top_countries = list(df['country_code'].value_counts()[df['country_code']
+    .value_counts() > df['country_code'].value_counts().mean()].index)
 
-def invert_dict(d):
-    """Function to invert a dictionary of lists so list values become keys."""
-    return dict( (v,k) for k in d for v in d[k] )
+    # Replace other countries with 'other'.
+    df['country_code'] = df['country_code'].fillna('other')
+    df.loc[~df['country_code'].isin(top_countries), 'country_code'] = 'other'
 
-category_replace_dict = invert_dict(top_50_dict)
+    # One-hot encoding of countries.
+    df = pd.concat([df, pd.get_dummies(df['country_code'], prefix='country')], axis=1)
+    df = df.drop('country_code', axis=1)
 
-# Relabel key value pairs that do not match well.
-category_replace_dict['big data analytics'] = 'big data'
-category_replace_dict['coworking'] = 'other'
-category_replace_dict['mining technologies'] = 'other'
-category_replace_dict['mobility'] = 'other'
-category_replace_dict['nanotechnology'] = 'other'
-category_replace_dict['real time'] = 'other'
-category_replace_dict['space travel'] = 'other'
-category_replace_dict['social network media'] = 'social media'
+    return df
 
-# Replace values in market column based on replacement dictionary.
-company_df['market'] = company_df['market'].map(category_replace_dict).fillna(company_df['market'])
+def impute_founding_date(df):
+    """Impute missing founding dates."""
+    # Convert datetime columns to datetime objects.
+    df['first_funding_at'] = pd.to_datetime(df['first_funding_at'], errors='coerce')
+    df['last_funding_at'] = pd.to_datetime(df['last_funding_at'], errors='coerce')
+    df['founded_at'] = pd.to_datetime(df['founded_at'], errors='coerce')
 
-# One-hot encoding of startup status.
-company_df['status'].value_counts()
+    # Save two date columns in new dataframe to calculate median value.
+    median_days_to_fund = np.median(((df['first_funding_at'] - df['founded_at'])
+    /(np.timedelta64(1, 'D'))).dropna())
 
-company_df = pd.concat([company_df, pd.get_dummies(company_df['status'], prefix='status')], axis=1)
-company_df = company_df.drop('status', axis=1)
+    # Impute NAs for founded_at by subtracting median_days_to_fund to first_funding_at.
+    df['founded_at'] = df['founded_at'].fillna(df['first_funding_at'] -
+    timedelta(days = median_days_to_fund))
 
-# Countries with startup numbers above global mean.
-top_countries = list(company_df['country_code'].value_counts()[company_df['country_code'].value_counts() > company_df['country_code'].value_counts().mean()].index)
+    # Filling in founded date features based on imputed values.
+    df['founded_year'] = df['founded_at'].dt.year
+    df['founded_month'] = df['founded_at'].dt.month
+    df['founded_quarter'] = df['founded_month'].map({1:1,2:1,3:1,4:2,5:2,6:2,
+    7:3,8:3,9:3,10:4,11:4,12:4})
 
-# Replace other countries with 'other'.
-company_df['country_code'] = company_df['country_code'].fillna('other')
-company_df.loc[~company_df['country_code'].isin(top_countries), 'country_code'] = 'other'
+    return df
 
-# One-hot encoding of countries.
-company_df = pd.concat([company_df, pd.get_dummies(company_df['country_code'], prefix='country')], axis=1)
-company_df = company_df.drop('country_code', axis=1)
-company_df.columns
+def temporal_features(df):
+    """Feature generation for temporal features."""
 
-# Impute missing founding dates.
-# Convert datetime columns to datetime objects.
-company_df['first_funding_at'] = pd.to_datetime(company_df['first_funding_at'], errors='coerce')
-company_df['last_funding_at'] = pd.to_datetime(company_df['last_funding_at'], errors='coerce')
-company_df['founded_at'] = pd.to_datetime(company_df['founded_at'], errors='coerce')
+    # Number of days and months from first founding date that first round was raised.
+    df['days_to_fund'] = ((df['first_funding_at'] - df['founded_at'])/
+    (np.timedelta64(1, 'D')))
+    df['months_to_fund'] = ((df['first_funding_at'] - df['founded_at'])/
+    (np.timedelta64(1, 'M')))
 
-# Save two date columns in new dataframe to calculate median value.
-median_days_to_fund = np.median(((company_df['first_funding_at'] - company_df['founded_at'])/(np.timedelta64(1, 'D'))).dropna())
+    # Number of days and months between funding rounds.
+    df['days_between_rounds'] = ((df['last_funding_at'] - df['first_funding_at'])/
+    (np.timedelta64(1, 'D'))/df['funding_rounds'])
+    df['months_between_rounds'] = ((df['last_funding_at'] - df['first_funding_at'])/
+    (np.timedelta64(1, 'M'))/df['funding_rounds'])
 
-# Impute NAs for founded_at by subtracting median_days_to_fund to first_funding_at.
-company_df['founded_at'] = company_df['founded_at'].fillna(company_df['first_funding_at'] - timedelta(days = median_days_to_fund))
+    # If only a single round, use value of days or months to first funding.
+    df.loc[df['days_between_rounds'] == 0, 'days_between_rounds'] =
+    df['days_to_fund']
+    df.loc[df['months_between_rounds'] == 0, 'months_between_rounds'] =
+    df['months_to_fund']
 
-# Filling in founded date features based on imputed values.
-company_df['founded_year'] = company_df['founded_at'].dt.year
-company_df['founded_month'] = company_df['founded_at'].dt.month
-company_df['founded_quarter'] = company_df['founded_month'].map({1:1,2:1,3:1,4:2,5:2,6:2,7:3,8:3,9:3,10:4,11:4,12:4})
+    return df
 
-# Feature generation for temporal features.
+def run_clean_companies(args):
+    """Loads config and cleans the companies dataset. Uses cleaned data to
+    generate new features.
+    Args:
+        args: From argparse, should contain args.config and optionally, args.save
+            args.config (str): Path to yaml file with load_data as a top level
+            key containing relevant configurations
+            args.save (str): If given, resulting dataframe will be saved to this
+            location.
+    Returns: None
+    """
+    with open(args.config, "r") as f:
+        config = yaml.load(f)
 
-# Number of days and months from first founding date that first round was raised.
-company_df['days_to_fund'] = (company_df['first_funding_at'] - company_df['founded_at'])/(np.timedelta64(1, 'D'))
-company_df['months_to_fund'] = (company_df['first_funding_at'] - company_df['founded_at'])/(np.timedelta64(1, 'M'))
+    df = read_data(args.input_file)
+    df = filter_columns(df, **config['clean_companies']['filter_columns'])
+    df = reduce_market_categories(df)
+    df = generate_onehot_features(df, config['clean_companies']
+    ['generate_onehot_features'])
+    df = company_country_features(df)
+    df = impute_founding_date(df)
+    df = temporal_features(df)
 
-# Number of days and months between funding rounds.
-company_df['days_between_rounds'] = (company_df['last_funding_at'] - company_df['first_funding_at'])/(np.timedelta64(1, 'D'))/company_df['funding_rounds']
-company_df['months_between_rounds'] = (company_df['last_funding_at'] - company_df['first_funding_at'])/(np.timedelta64(1, 'M'))/company_df['funding_rounds']
+    df.to_csv(args.save)
 
-# If only a single round, use value of days or months to first funding.
-company_df.loc[company_df['days_between_rounds'] == 0, 'days_between_rounds'] = company_df['days_to_fund']
-company_df.loc[company_df['months_between_rounds'] == 0, 'months_between_rounds'] = company_df['months_to_fund']
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('--config', help='path to yaml file with configurations')
+    parser.add_argument('--input_file', help='path to csv file with raw data')
+    parser.add_argument('--save', default='data/auxiliary',
+    help='path to where the cleaned dataset should be saved to')
 
-company_df.shape
+    args = parser.parse_args()
 
-# Export list of unique company keys as csv.
-company_df['permalink'].to_csv('./data/auxiliary/filtered_company.csv')
+    run_clean_companies(args)
 
-# Export list of unique markets as csv.
-pd.DataFrame(list(company_df['market'].unique())).to_csv('./data/auxiliary/filtered_markets.csv')
+# makefile command
+# python src/clean_companies_data.py --config=config/model_config.yml --input_file=data/external/companies.csv --save=data/auxiliary/
