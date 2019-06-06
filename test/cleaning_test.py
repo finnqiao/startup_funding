2e2cd3296a44a5d5e76a28641dc5420f9548f8c1
finnqiao <yu.qiao@tufts.edu>
2019-06-06 11:14:07 -0500

    Testing functions for acquisition and investments

--- test/cleaning_test.py
+++ test/cleaning_test.py
@@ -6,6 +6,9 @@ import sys
 import logging
 sys.path.append(os.path.abspath('./src'))
 from clean_companies_data import reduce_market_categories, company_country_features, impute_founding_date, temporal_features
+from clean_acquisitions_data import get_acquisition_count
+from clean_investors_data import bin_investors, get_unique_investors
+from clean_rounds_data import group_permalink_funding
 
 logging.basicConfig(level=logging.DEBUG, filename="test_logfile", filemode="a+",
                     format="%(asctime)-15s %(levelname)-8s %(message)s")
@@ -13,6 +16,9 @@ logger = logging.getLogger(__name__)
 
 # Read in data to test with.
 companies_df = pd.read_csv('data/external/companies.csv')
+acquisitions_df = pd.read_csv('data/external/acquisitions.csv')
+investments_df = pd.read_csv('data/external/investments.csv')
+rounds_df = pd.read_csv('data/external/rounds.csv')
 
 final_num_markets = 52
 final_num_countries = 10
@@ -43,3 +49,23 @@ def test_temporal_types():
     df = temporal_features(df)
     new_features = df.shape[1]
     assert((new_features - initial_features) == final_num_temporal)
+
+# Test to see if all unique acquirers.
+def test_unique_acquirers():
+    df = get_acquisition_count(acquisitions_df, 'data/external/companies.csv')
+    acq_list = list(df['permalink'])
+    assert(len(acq_list) == len(set(acq_list)))
+
+# Test to check whether labels are in deciles.
+def test_investor_bins():
+    df = bin_investors(investments_df)
+    assert(len(list(df['bins_amount'].unique())) == 11)
+
+# Test to check whether companies belong to initial company list.
+def test_unique_investors():
+    df = bin_investors(investments_df)
+    df = get_unique_investors(df, 'data/external/companies.csv')
+
+    all_companies_list = list(companies_df['permalink'].unique())
+    invested_companies_list = list(df['company_permalink'].unique())
+    assert(set(invested_companies_list).issubset(all_companies_list) == True)
